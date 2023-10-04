b886b642ebe9bda5681d2885499d3e678bcd56d7
Zack Stout <zackstout@gmail.com>
2023-10-04 16:33:17 -0500

    Basic html working..

--- index.js
+++ index.js
@@ -4,6 +4,84 @@ const puppeteer = require("puppeteer");
 const path = require("path");
 const downloadPath = path.resolve("./download");
 const { setTimeout } = require("node:timers/promises");
+const express = require("express");
+const bodyParser = require("body-parser");
+const pdf = require("pdf-parse");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + ".pdf"); //Appending .jpg
+    },
+  }),
+});
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+    parameterLimit: 1000000,
+  })
+);
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.raw());
+
+// app.use(express.json({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb'}));
+
+app.listen(PORT, () => {
+  console.log("Thx for listening on", PORT);
+});
+
+app.post("/pdf", upload.single("file"), (req, res) => {
+  //   console.log(
+  //     "Got file..",
+  //     Object.keys(req.body).length,
+  //     Object.values(req.body)
+  //   );
+  //   useSmallPdfToUnlockFile(req.body);
+
+  console.log("uploaded files....??");
+
+  //   const raw = JSON.stringify(req.body);
+
+  //   let buff = Buffer.from(raw, "base64");
+
+  //   useSmallPdfToUnlockFile(raw);
+
+  //   fs.writeFile("./download/testing_this_out.pdf", buff, () => {
+  //     console.log("done....");
+  //   });
+
+  //   pdf(buff).then((data) => {
+  //     console.log("data from pdf...", data, "and buff...", buff);
+  //     fs.writeFile("./download/testing_this_out.pdf", data, () => {
+  //       console.log("done....");
+  //     });
+  //   });
+
+  res.sendStatus(200);
+});
+
+/**
+ * TODO...
+ *
+ * - Need to deploy a server app... somehow
+ * - Need frontend to accept, idk, title, and then upload the pdf file.
+ * - Then we need to send that file through our "useSmallPdf" function,
+ * download that file,
+ * and then send that into our splitPdf function.
+ * - And then we need to download all those files... on the client... as a zip? Idk...
+ */
 
 const desiredSubdocumentsRaw = `1-3: Contact Information
 4: IPP Participation Agreement
@@ -38,15 +116,11 @@ async function splitPdf(pathToPdf) {
   const docmentAsBytes = await fs.promises.readFile(pathToPdf);
 
   // Load your PDFDocument
-  // Oh, we can just... ignore encryption??
-  const pdfDoc = await PDFDocument.load(docmentAsBytes, {
-    ignoreEncryption: true,
-  });
-
-  //   const numberOfPages = pdfDoc.getPages().length;
+  // Oh, we can just... ignore encryption?? No... it doesn't work. Prints blank pages haha.
+  const pdfDoc = await PDFDocument.load(docmentAsBytes);
 
   for (const { title, range } of desiredSubdocuments) {
-    console.log("title", title, range);
+    // console.log("title", title, range);
     const subDocument = await PDFDocument.create();
     const copiedPages = await subDocument.copyPages(pdfDoc, range);
     for (const p of copiedPages) {
@@ -66,7 +140,8 @@ function writePdfBytesToFile(fileName, pdfBytes) {
 /**
  * Absolutely cannot believe that this works lol
  */
-async function simplefileDownload() {
+async function useSmallPdfToUnlockFile(pdfFileToUnlock) {
+  console.log("we have called usesmallpdf");
   const browser = await puppeteer.launch({
     headless: false,
   });
@@ -77,7 +152,12 @@ async function simplefileDownload() {
   });
 
   const elementHandle = await page.$("input[type=file]");
-  await elementHandle.uploadFile("./test.pdf");
+
+  // OOooh this HAS to be a string.... so I guess we have to download the file.... to fs....
+  //   const fileToUpload = "./test.pdf";
+  const fileToUpload = pdfFileToUnlock;
+
+  await elementHandle.uploadFile(fileToUpload);
 
   await page.waitForSelector("input[type=checkbox]");
 
@@ -122,10 +202,10 @@ async function simplefileDownload() {
 }
 
 const run = async () => {
-  //   simplefileDownload();
+  //   useSmallPdfToUnlockFile();
   await splitPdf("test-unlocked.pdf");
 
   //   console.log(desiredSubdocuments);
 };
 
-run();
+// run();
